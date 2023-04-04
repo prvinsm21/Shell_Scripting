d699dfa696425bdc6ab5e72395ded84c2867cc23
Macko <prvinsm20@gmail.com>
2023-04-04 13:45:38 +0530

    added files

---
+++ Practice_scripts/Functions.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+# 3 type of Function type written below
+
+function install() {
+    # code for installation
+    echo "Software installed"
+}
+install
+
+configuration() {
+
+    echo "Configuration done"
+}
+configuration
+
+function deploy {
+    echo "Application deployed"
+}
+deploy
--- Practice_scripts/default_value.sh
+++ Practice_scripts/default_value.sh
@@ -5,3 +5,8 @@ read -p "Please enter your name: " NAME
 NAME=${NAME:-World} # If Parameter not passed, It will take World as default
 
 echo "Hello ${NAME^}" # Here ^ represent 1st Letter to be in CAPS
+
+yourname=${unsetvariable-Macko}
+echo $yourname
+
+ 
